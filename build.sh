7ed0a644f1fdd8f9fc02f0294c093462f2f5d134
Dustin Rinne <dustin.rinne@gmail.com>
2018-01-05 23:15:27 -0600

    4.1 test

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ export CROSS_COMPILE=${HOME}/Downloads/aarch64-linux-android-4.9/bin/aarch64-lin
 DEFCONFIG="rey_defconfig"
 
 # Kernel Details
-VER=".ph1-clang-r4.1-oc"
+VER=".ph1-clang-r4.1-oc-test"
 
 # Paths
 KERNEL_DIR=`pwd`
