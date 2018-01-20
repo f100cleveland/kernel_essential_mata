161180abedfaea30a6de25312373c7a3a9b216ac
Dustin Rinne <dustin.rinne@gmail.com>
2018-01-20 00:24:12 -0600

    build 4.4.112 with td framework

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ export CLANG_PATH=${HOME}/Downloads/dtc7/bin/
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${HOME}/Downloads/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-DEFCONFIG="rey_defconfig"
+DEFCONFIG="fuckery_defconfig"
 
 # Kernel Details
-VER=".ph1-dtc7-4.4.112-polly"
+VER=".ph1-dtc7-4.4.112-polly-td"
 
 # Paths
 KERNEL_DIR=`pwd`
