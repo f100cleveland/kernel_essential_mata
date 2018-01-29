3815481c805da36e2b4dd81fd65294276b09c6f9
Dustin Rinne <dustin.rinne@gmail.com>
2018-01-29 16:47:06 -0600

    build 5.3

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ export CROSS_COMPILE=${HOME}/Downloads/aarch64-linux-android-4.9/bin/aarch64-lin
 DEFCONFIG="fuckery_defconfig"
 
 # Kernel Details
-VER=".ph1-dtc7-5.2"
+VER=".ph1-dtc7-5.3"
 
 # Paths
 KERNEL_DIR=`pwd`
